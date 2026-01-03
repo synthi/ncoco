88d16dc61fa27a4f45cf8e9b877655fcbeee234d
Josue Arias <31876967+synthi@users.noreply.github.com>
2026-01-03 17:47:10 -0300

    Create param_set.lua

---
+++ lib/param_set.lua
@@ -0,0 +1,122 @@
+-- lib/param_set.lua v2002
+-- CHANGELOG v2002:
+-- 1. FIX: 'load_tape' now uses correct syntax params:add{type="file"...}.
+--    This fixes manual loading issues.
+
+local Params = {}
+
+function Params.init(SC, G)
+  -- 1. GLOBALS & TAPE
+  params:add_separator("GLOBALS")
+  params:add_control("global_vol", "Master Vol", controlspec.new(0, 2, "lin", 0, 1))
+  params:set_action("global_vol", function(x) SC.set_amp(1,x); SC.set_amp(2,x) end)
+  
+  params:add_control("global_chaos", "Global Chaos", controlspec.new(0, 1, "lin", 0, 0))
+  params:set_action("global_chaos", function(x) for i=1,6 do params:set("p"..i.."chaos", x) end end)
+  
+  params:add_option("skip_mode", "Skip Mode", {"Stutter", "Chaos"}, 1)
+  params:set_action("skip_mode", function(x) engine.skipMode(x-1) end)
+
+  params:add_group("TAPE OPS", 5)
+  params:add_option("tape_target", "Target", {"Left", "Right", "Both"}, 3)
+  params:add_control("tape_len", "Loop Length", controlspec.new(0.1, 60, "lin", 0.1, 8.0, "s"))
+  params:set_action("tape_len", function(x) SC.set_loop_len(x) end)
+  
+  -- STORAGE ACTIONS
+  params:add_trigger("save_tape", "Save New Tape")
+  params:set_action("save_tape", function() if params.action_write then params.action_write() end end)
+  
+  -- MANUAL LOAD FIX (CORRECT SYNTAX)
+  params:add{type="file", id="load_tape", name="Load Tape", path=_path.audio, action=function(path) 
+    if path and path ~= "cancel" and path ~= "" then
+       print("Manual Load: "..path)
+       local t = params:get("tape_target")
+       if t==1 or t==3 then engine.read_tape(1, path) end -- 1=L
+       if t==2 or t==3 then engine.read_tape(2, path) end -- 2=R
+    end
+  end}
+  
+  params:add_trigger("clear_tape", "Clear Tape")
+  params:set_action("clear_tape", function() 
+    local t = params:get("tape_target")
+    if t==1 or t==3 then engine.clear_tape(0) end
+    if t==2 or t==3 then engine.clear_tape(1) end
+  end)
+
+  -- 2. COCO CHANNEL STRIPS
+  for i=1, 2 do
+    local s = (i==1) and "L" or "R"
+    local num = (i==1) and "1" or "2"
+    params:add_group("COCO "..num, 11) 
+    
+    params:add_control("preamp"..s, "Preamp "..num, controlspec.new(1.0, 20.0, "lin", 0, 1.0))
+    params:set_action("preamp"..s, function(x) SC.set_preamp(i,x) end)
+    params:add_control("envSlew"..s, "Env Slew "..num, controlspec.new(0.001, 1.0, "exp", 0, 0.05))
+    params:set_action("envSlew"..s, function(x) SC.set_env_slew(i,x) end)
+    
+    params:add_control("speed"..s, "Speed "..num, controlspec.new(0.001, 3.0, "lin", 0, 1.0))
+    params:set_action("speed"..s, function(x) SC.set_speed(i, x) end)
+    params:add_control("fb"..s, "Feedback "..num, controlspec.new(0, 1.2, "lin", 0, 0.85))
+    params:set_action("fb"..s, function(x) SC.set_feedback(i,x) end)
+    params:add_control("filt"..s, "Filter "..num, controlspec.new(-1, 1, "lin", 0, 0))
+    params:set_action("filt"..s, function(x) SC.set_filter(i,x) end)
+    
+    params:add_option("bits"..s, "Bits "..num, {"8bit", "12bit", "16bit"}, 1)
+    params:set_action("bits"..s, function(x) 
+       local b = (x==1) and 8 or ((x==2) and 12 or 16)
+       SC.set_bitdepth(i,b) 
+    end)
+    
+    local dolby_opts = {"Off", "Gate In", "Gate Fb", "Gate All", "Punch In", "Punch Out", "G-In/P-In", "P-Out/G-Fb", "Cross Duck"}
+    params:add_option("dolby"..s, "Dolby "..num, dolby_opts, 5) 
+    params:set_action("dolby"..s, function(x) SC.set_dolby(i, x-1) end)
+    
+    params:add_binary("rec"..s, "Rec "..num, "toggle", 0)
+    params:set_action("rec"..s, function(x) SC.set_rec(i,x) end)
+    params:add_binary("flip"..s, "Flip "..num, "toggle", 0)
+    params:set_action("flip"..s, function(x) SC.set_flip(i, x) end)
+    params:add_binary("skip"..s, "Skip "..num, "momentary", 0)
+    params:set_action("skip"..s, function(x) SC.set_skip(i,x) end)
+    
+    params:add_control("pan"..((i==1) and "_l" or "_r"), "Pan "..num, controlspec.new(-1, 1, "lin", 0, (i==1) and -0.5 or 0.5))
+    params:set_action("pan"..((i==1) and "_l" or "_r"), function(x) SC.set_pan(i,x) end)
+  end
+  
+  -- 3. HEXAQUANTUS
+  for i=1, 6 do
+    params:add_group("PETAL "..i, 5)
+    
+    params:add_option("p"..i.."range", "Range", {"LFO", "Audio"}, 1)
+    params:set_action("p"..i.."range", function(x)
+       if x == 1 then
+         params:show("p"..i.."f_lfo")
+         params:hide("p"..i.."f_aud")
+         SC.set_petal_freq(i, params:get("p"..i.."f_lfo"))
+       else
+         params:hide("p"..i.."f_lfo")
+         params:show("p"..i.."f_aud")
+         SC.set_petal_freq(i, params:get("p"..i.."f_aud"))
+       end
+       _menu.rebuild_params()
+    end)
+
+    params:add_control("p"..i.."f_lfo", "Freq (LFO)", controlspec.new(0.01, 20, "exp", 0, 1))
+    params:set_action("p"..i.."f_lfo", function(x) 
+      if params:get("p"..i.."range") == 1 then SC.set_petal_freq(i,x) end 
+    end)
+    
+    params:add_control("p"..i.."f_aud", "Freq (Aud)", controlspec.new(20, 2000, "exp", 0, 200))
+    params:set_action("p"..i.."f_aud", function(x) 
+      if params:get("p"..i.."range") == 2 then SC.set_petal_freq(i,x) end 
+    end)
+    params:hide("p"..i.."f_aud") 
+
+    params:add_control("p"..i.."chaos", "Chaos", controlspec.new(0, 1, "lin", 0, 0))
+    params:set_action("p"..i.."chaos", function(x) SC.set_petal_chaos(i,x) end)
+    
+    params:add_option("p"..i.."shape", "Shape", {"Tri", "Castle"}, 1)
+    params:set_action("p"..i.."shape", function(x) SC.set_petal_shape(i,x-1) end)
+  end
+end
+
+return Params
